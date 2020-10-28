47f4ad0efed28d89e6214c71228ef916f777518e
sabin <sabin.mimulici@gmail.com>
2020-10-28 18:37:30 +0100

    added lib dep in cmake

---
+++ build/CMakeFiles/server_randno.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/server_randno.dir/server.c.o"
+  "server_randno"
+  "server_randno.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/server_randno.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
